0648e1c85300643f0578d97a896e4a4f9e2456ae
natenaranjo <46088263+natenaranjo@users.noreply.github.com>
2022-10-09 16:43:25 +0000

    Deploying to gh-pages from @ natenaranjo/web3-portfolio@b49621b0faa87c4ab46104a432f8e491adef5fe9 🚀

---
+++ _next/static/chunks/d64684d8-bee5b594e9850c24.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[556],{3854:function(t,n,r){r.d(n,{Ba7:function(){return o}});var e=r(8357);function o(t){return(0,e.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M5 11l7-7 7 7M5 19l7-7 7 7"}}]})(t)}}}]);
