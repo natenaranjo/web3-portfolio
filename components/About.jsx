f8e6c112f445ccc1697ec371eeec59d25c6b791e
Nate Naranjo <46088263+natenaranjo@users.noreply.github.com>
2022-10-09 14:12:09 -0500

    Changed the image size

--- components/About.jsx
+++ components/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
                 </Link>
             </div>
             <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-                <Image className='rounded-xl' src='/assets/about.png' alt='/' width='250' height='250' />
+                <Image className='rounded-xl' src='/assets/about.png' alt='/' width='650' height='650' />
             </div>
         </div>
     </div>
