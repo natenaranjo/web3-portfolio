7611e63f2ded715ae772119272aa78865919dbb9
Nate Naranjo <naranjo_nathaniel@live.com>
2022-10-09 14:07:07 -0500

    removed the loader

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  basePath: "/web3-portfolio",
-  assetPrefix: "/web3-portfolio",
 }
 
 module.exports = nextConfig
